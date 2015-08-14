4ce4f47bad4c785db963a4c02fca4400a7acae79
Stuart Chinery <stuart.chinery@headlondon.com>
2015-08-14 17:25:52 +0100

    Added Cache class.

---
+++ test/lib/cache.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var Cache = require('../../lib/cache');
+// var sinon = require('sinon');
+
+var cache = null;
+
+describe('Authentication', function() {
+  describe('initalising', function() {
+    beforeEach(function() {
+      cache = new Cache();
+    });
+
+    it.skip('should do something', function() {
+    });
+  });
+
+  describe('methods', function() {
+    describe('read', function() {
+      it.skip('should do something', function() {
+      });
+    });
+
+    describe('save', function() {
+      it.skip('should do something', function() {
+      });
+    });
+  });
+});
